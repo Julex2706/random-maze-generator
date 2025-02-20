99adf673cd906338818e422a24cdfa960788f5ce
Julex2706 <juliankuhlen@gmail.com>
2025-02-20 16:43:37 +0000

    Update myze.py

--- maze.py
+++ maze.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
-import __future__
+from __future__ import print_function
 
-help(__future__)
+import random
+import sys
+
+EMPTY = ' '
+WALL = '#'
+AGENT = 'o'
+GOAL = 'x'
+
+def adjacent(cell):
+    i,j = cell
+    for (y,x) in ((1,0), (0,1), (-1,0), (0,-1)):
+        yield (i+y, j+x), (i+2*y, j+2*x)
+
